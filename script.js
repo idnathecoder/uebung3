66eca45c19b03c3d43f4dafd31797a9c49ebb5f3
idnathecoder <andreas.geisler1908@gmx.at>
2024-12-24 13:02:51 +0100

    commit4

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ class Ratte {
 
         //TODO change position according to width and according to height.
         this.position = {
-            x: 100,
-            y: 100  
+            x: 500,
+            y: 200  
         }
         
         this.velocity = {
@@ -36,8 +36,62 @@ class Ratte {
         player.previousPositions.x = []
         player.previousPositions.y = []
     }
-    
 
+    handleCollision(obstacle){
+        // Rechte Seite des Hindernisses
+        if (
+            this.position.x - this.radius <= obstacle.position.x + obstacle.width &&
+            this.position.x - this.radius > obstacle.position.x + obstacle.width - 10 &&
+            (this.position.y + this.radius > obstacle.position.y &&
+            this.position.y - this.radius < obstacle.position.y + obstacle.height)
+        ) {
+            this.velocity.x *= -1;
+            this.position.x += 1;
+            //console.log("rechts")
+        }
+
+        // Linke Seite des Hindernisses
+        if (
+            this.position.x + this.radius >= obstacle.position.x &&
+            this.position.x + this.radius < obstacle.position.x + 10 &&
+            (this.position.y + this.radius > obstacle.position.y &&
+            this.position.y - this.radius < obstacle.position.y + obstacle.height)
+        ) {
+            this.velocity.x *= -1;
+            this.position.x -= 1;
+            //console.log("links")
+
+        }
+
+        // Obere Seite des Hindernisses
+        if (
+            this.position.y + this.radius >= obstacle.position.y &&
+            this.position.y + this.radius < obstacle.position.y + 10 &&
+            (this.position.x + this.radius > obstacle.position.x &&
+            this.position.x - this.radius < obstacle.position.x + obstacle.width)
+        ) {
+            this.velocity.y *= -1;
+            this.position.y -= 1;
+            //console.log("oben")
+
+        }
+
+        // Untere Seite des Hindernisses
+        if (
+            this.position.y - this.radius <= obstacle.position.y + obstacle.height &&
+            this.position.y - this.radius > obstacle.position.y + obstacle.height - 10 &&
+            (this.position.x + this.radius > obstacle.position.x &&
+            this.position.x - this.radius < obstacle.position.x + obstacle.width)
+        ) {
+            this.velocity.y *= -1;
+            this.position.y += 1;
+            //console.log("unten")
+
+        }
+
+
+    }
+    
     drawLine(){
         c.beginPath();
         c.moveTo(this.previousPositions.x[0], this.previousPositions.y[0]); // Start at the previous position
@@ -60,17 +114,17 @@ class Ratte {
     update(){
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
-        this.velocity.x *= 0.96
-        this.velocity.y *= 0.96
+        this.velocity.x *= 0.97 //0.96
+        this.velocity.y *= 0.97
+
+        this.handleCollision(obstacle1)
 
         this.draw()
         if(this.recordposition === 1){
-            console.log(this.previousPositions.x.length)
             if(this.previousPositions.x.length >= 10){
                 for(let counter = 0; counter < this.previousPositions.x.length-1; counter++){
                     this.previousPositions.x[counter] = this.previousPositions.x[counter+1]
                     this.previousPositions.y[counter] = this.previousPositions.y[counter+1]
-                    console.log(counter)
                 }
                 this.previousPositions.x.pop()
                 this.previousPositions.y.pop()
@@ -83,6 +137,29 @@ class Ratte {
     }
 }
 
+class Obstacle {
+    constructor(width, height, posx, posy) {
+        this.width = width
+        this.height = height
+        this.color = "red"
+
+        
+
+        //TODO change position according to width and according to height.
+        this.position = {
+            x: posx,
+            y: posy
+        }
+        
+    }
+    draw(){
+        c.beginPath()           
+        c.rect(this.position.x, this.position.y, this.width, this.height)
+        c.fillStyle = this.color
+        c.fill()        
+    }
+}
+
 function calcboost(time){
     let a = 5
     let b = 0.5
@@ -91,7 +168,7 @@ function calcboost(time){
     if (time < c){ 
         let n = Math.log(a/b)/Math.log(2)
         let m = a/(Math.pow(c,n))
-        console.log(m*Math.pow(time,n))
+        //console.log(m*Math.pow(time,n))
         if(m*Math.pow(time,n) > a -1){
             player.startLine()
         } else{
@@ -102,7 +179,7 @@ function calcboost(time){
         
     } else {
         player.startLine()
-        console.log(a)
+        //console.log(a)
         return a
     }
 }
@@ -124,11 +201,14 @@ addEventListener("click", ({offsetX, offsetY}) => {
 })
 
 const player = new Ratte()
+const obstacle1 = new Obstacle(200, 200, 100, 100)
+
 
 function animate(){
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
     player.draw()
+    obstacle1.draw()
 
     requestAnimationFrame(animate)
 }
