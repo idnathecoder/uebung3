bedf407606ef64c31d7e7ef53a500ad3367e6bb0
idnathecoder <andreas.geisler1908@gmx.at>
2024-12-24 10:38:55 +0100

    commit3

--- script.js
+++ script.js
@@ -16,24 +16,47 @@ class Ratte {
             x: 0,
             y: 0,    
         }
+
+        this.recordposition = 0
+        this.previousPositions = {
+            x: this.x = [],
+            y: this.y = []
+        }
     }
 
-    drawTrail() {
-        // Draw a line from the previous position to the current position
+    startLine(){
+        this.recordposition = 1
+    }
+
+    endLine(){
+        this.recordposition = 0
+    }
+
+    clearpreviousPositions(){
+        player.previousPositions.x = []
+        player.previousPositions.y = []
+    }
+    
+
+    drawLine(){
         c.beginPath();
-        c.moveTo(this.previousPosition.x, this.previousPosition.y); // Start at the previous position
+        c.moveTo(this.previousPositions.x[0], this.previousPositions.y[0]); // Start at the previous position
+        for(const counter in this.previousPositions.x){
+            c.lineTo(this.previousPositions.x[counter], this.previousPositions.y[counter]); // Draw to the current position
+        }
         c.lineTo(this.position.x, this.position.y); // Draw to the current position
         c.strokeStyle = "white"; // Line color
         c.lineWidth = 2; // Line thickness
         c.stroke();
     }
-    
+
     draw(){
         c.beginPath()
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
         c.fillStyle = this.color
-        c.fill()        
+        c.fill()
     }
+
     update(){
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -41,25 +64,44 @@ class Ratte {
         this.velocity.y *= 0.96
 
         this.draw()
-        //this.drawTrail()
+        if(this.recordposition === 1){
+            console.log(this.previousPositions.x.length)
+            if(this.previousPositions.x.length >= 10){
+                for(let counter = 0; counter < this.previousPositions.x.length-1; counter++){
+                    this.previousPositions.x[counter] = this.previousPositions.x[counter+1]
+                    this.previousPositions.y[counter] = this.previousPositions.y[counter+1]
+                    console.log(counter)
+                }
+                this.previousPositions.x.pop()
+                this.previousPositions.y.pop()
+            } else {
+                this.previousPositions.x.push(this.position.x)
+                this.previousPositions.y.push(this.position.y) 
+            }
+            this.drawLine()
+        }
     }
 }
 
 function calcboost(time){
     let a = 5
-    let b = 1
+    let b = 0.5
     let c = 1/6
     time /= 1000
-    console.log("test", 1/time)
     if (time < c){ 
-        
         let n = Math.log(a/b)/Math.log(2)
         let m = a/(Math.pow(c,n))
         console.log(m*Math.pow(time,n))
+        if(m*Math.pow(time,n) > a -1){
+            player.startLine()
+        } else{
+            player.clearpreviousPositions()
+        }
+        
         return m*Math.pow(time,n)
         
-
     } else {
+        player.startLine()
         console.log(a)
         return a
     }
@@ -67,10 +109,9 @@ function calcboost(time){
 
 let lastclick = Date.now()
 addEventListener("click", ({offsetX, offsetY}) => {
-    
     //console.log(offsetX-player.position.x)
 
-
+    player.endLine()
 
     let alpha = Math.atan2((offsetY-player.position.y),(offsetX-player.position.x))
     let boost = calcboost(Date.now()-lastclick)
@@ -78,26 +119,18 @@ addEventListener("click", ({offsetX, offsetY}) => {
     
     player.velocity.x += Math.cos(alpha)*boost
     player.velocity.y += Math.sin(alpha)*boost
-    if(boost > 5){
-        player.drawTrail()
-    }
 
     lastclick = Date.now()
-
-    
-    
 })
 
 const player = new Ratte()
 
 function animate(){
-    
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
     player.draw()
 
     requestAnimationFrame(animate)
-    
 }
 
 animate()
